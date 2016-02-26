31f1636b31e1ba8728a7e6d4fe09ba1d811566eb
Meta-link <Meta-link@hotmail.fr>
2016-02-26 12:58:09 +0100

    topkek

---
+++ Assets/Scripts/EnnemyScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnnemyScript : MonoBehaviour {
+
+    private NavMeshAgent nav;
+
+	// Use this for initialization
+	void Start () {
+        nav = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        nav.SetDestination(GameObject.Find("Mouton_Gameplay").transform.position);
+	}
+}
