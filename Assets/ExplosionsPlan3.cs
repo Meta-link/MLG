2052ca54d9d999cd1fe452076a684c228db1453a
Meta-link <Meta-link@hotmail.fr>
2016-02-26 15:06:20 +0100

    Final commit

---
+++ Assets/ExplosionsPlan3.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplosionsPlan3 : MonoBehaviour {
+
+    private GameObject explosions;
+
+	// Use this for initialization
+	void Start () {
+        explosions = GameObject.Find("EXPLOSIONS");
+        explosions.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Time.realtimeSinceStartup > 1.5)
+        {
+            explosions.SetActive(true);
+        }
+	}
+}
